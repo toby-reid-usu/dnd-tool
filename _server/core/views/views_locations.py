c91bb5d7b67bb37b724ee1ba50afc0232fe76d69
Toby Reid <toby.reid@usu.edu>
2024-12-13 17:56:09 -0700

    fixing breaks to Locations

--- _server/core/views/views_locations.py
+++ _server/core/views/views_locations.py
@@ -28,52 +28,23 @@ def locations(req: HttpRequest, campaign_id: int) -> HttpResponse:
 
 
 # /campaigns/<int:campaign_id>/locations/new/
-# @login_required
-# def locations_new(req: HttpRequest, campaign_id: int) -> HttpResponse:
-#     campaign_opt = get_campaign_opt(campaign_id, req.user)
-#     if isinstance(campaign_opt, HttpResponse):
-#         return campaign_opt
-
-#     if req.method == "GET":
-#         context = {
-#             ASSET: ASSET_URL,
-#             CSS: CSS_FILE,
-#             CURRENT_USER: req.user,
-#             CURRENT_CAMPAIGN: campaign_opt,
-#             LOCATIONS: Location.objects.filter(campaign=campaign_opt),
-#         }
-#         return render(req, "campaigns/locations/new.html", context)
-#     # else it's POST, and it's a new location request
-#     return location_form(req, campaign_opt)
-
-### My version ###
 @login_required
-def locations_new(request, campaign_id):
-    campaign = get_object_or_404(Campaign, id=campaign_id)
+def locations_new(req: HttpRequest, campaign_id: int) -> HttpResponse:
+    campaign_opt = get_campaign_opt(campaign_id, req.user)
+    if isinstance(campaign_opt, HttpResponse):
+        return campaign_opt
 
-    if request.method == "POST":
-        form = LocationForm(request.POST)
-        if form.is_valid():
-            # Save the location object first
-            location = form.save(commit=False)
-            location.campaign = campaign  # Assuming Location has a campaign ForeignKey
-            location.save()
-            form.save_m2m()
-
-            # Add neighboring locations (if any)
-            neighbors = form.cleaned_data.get("neighboring_locations")
-            if neighbors:
-                location.neighboring_locations.set(neighbors)
-
-            return redirect("campaign_detail", campaign_id=campaign.id)
-        else:
-            return HttpResponseBadRequest("Invalid data in form")
-    else:
-        form = LocationForm()
-    return render(request, "locations/new.html", {"form": form, "campaign": campaign})
-
-### ###
-
+    if req.method == "GET":
+        context = {
+            ASSET: ASSET_URL,
+            CSS: CSS_FILE,
+            CURRENT_USER: req.user,
+            CURRENT_CAMPAIGN: campaign_opt,
+            LOCATIONS: Location.objects.filter(campaign=campaign_opt),
+        }
+        return render(req, "campaigns/locations/new.html", context)
+    # else it's POST, and it's a new location request
+    return location_form(req, campaign_opt)
 
 
 # /campaigns/<int:campaign_id>/locations/<int:location_id>/
