6927f34c30c3fd9de3c11ed9c7bd1b31eb368cd6
Toby Reid <toby.reid@usu.edu>
2024-12-13 19:33:27 -0700

    removing extraneous portions

--- _server/registration/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('sign_in/', views.sign_in),
-    path('sign_up/', views.sign_up),
-    path('logout/', views.logout_view),
-]
